881ac16715489c3740987801036b605bc5081811
NelsonPython <34105635+NelsonPython@users.noreply.github.com>
2019-11-19 21:16:38 -0800

    Update findCode.py

--- findCode.py
+++ findCode.py
@@ -1,19 +1,23 @@
 import os
 import re
 
-# store the list of files in a list and remove the end of filename character 
 fo = open("foundCode.txt","w")
+
+# store filenames in a list and remove the end of filename character 
 with open("docsFiles.txt") as f:
     filenames = f.read().split('\n')
 for filename in filenames:
     if len(filename) > 0:
-        filename = "/home/iotahub/"+filename
+        filename = "/home/$USERNAME/"+filename
         print("\n", filename, file=fo)
+
         # open each file in the list and read the contents into a string called txt
         g = open(filename,"r")
         txt = g.read()
+
         # find every instance of the formatting character (or marker) that designates code
         marks  = [m.start() for m in re.finditer('```',txt)]
+
         # if there are an even number of markers then gather the code between the markers
         if len(marks)%2==0:
             it = iter(marks)
