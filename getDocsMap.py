82d2acaabc7cedabcbe2ab0fca996a80415fe54f
NelsonPython <34105635+NelsonPython@users.noreply.github.com>
2019-11-19 20:52:56 -0800

    Add files via upload

---
+++ getDocsMap.py
@@ -0,0 +1,43 @@
+'''
+Read the documentation in a folder and get a list of subfolders and files
+'''
+
+import os
+
+testFolder = 'test/docsFolders.txt'
+testFile = 'test/docsFiles.txt'
+testDir = 'documentation/'
+
+
+#testFolder = "testNGG/docsFolder.txt"
+#testFile = "testNGG/docsFile.txt"
+#testDir = "AstroPiOTA"
+
+f = open(testFile,'w')
+ff = open(testFolder, 'w')
+
+for dirname, dirnames, filenames in os.walk(testDir):
+        for subdirname in dirnames:
+                if "git" not in dirname:
+                        if "git" not in subdirname:
+                                print(os.path.join(dirname, subdirname))
+                                print(os.path.join(dirname, subdirname), file=ff)
+
+        for filename in filenames:
+                if 'md'in filename:
+                        if "git" not in dirname:
+                                dirfile = dirname+"/"+filename
+                                print(dirfile, file=f)
+                                print(dirfile)
+                if 'how-to-' in dirname:
+                        if 'md' in filename:
+                                print(os.path.join(dirname,filename), file=ff)
+                                #print(os.path.join(dirname,filename))
+
+                if 'htm' in filename:
+                        print(os.path.join(dirname,filename), file=ff)
+                        #print(os.path.join(dirname,filename))
+
+f.close()
+ff.close()
+
